7bcb213386219c950cfa13e90edceec46515b495
Ryan McCrory <ryanmccrory83@gmail.com>
2018-10-02 11:38:43 -0600

    getting branches up to date

--- src/Auth/AuthService.js
+++ src/Auth/AuthService.js
@@ -19,10 +19,10 @@ export default class AuthService {
     auth0 = new auth0.WebAuth({
       domain: 'reach-out.auth0.com',
       clientID: 'auRV5jvDoeuBweySL4xIMxYtzlszrE3v',
-      //local developement callback//
       // redirectUri: 'https://reachout-91799.firebaseapp.com/callback' || 'http://localhost:3000/callback',
       //production callback//
       redirectUri: 'http://localhost:8080/callback' || 'http://localhost:3000/callback',
+      //local developement callback//
       responseType: 'token id_token',
       scope: 'openid'
     })
